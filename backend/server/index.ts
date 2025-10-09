ab7aec721c35186f618a5ae67ee22c3fee725849
aapani-dukan <shivraj8404@gmail.com>
2025-10-09 17:00:37 +0000

    Feat: Update CLIENT_URL fallback in backend CORS configuration

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -23,7 +23,8 @@ let server: Server;
 // CLIENT_URL को सीधे environment variable से पढ़ें, क्योंकि यह अब production के लिए ही होगा
 // Production में, यह Frontend का URL होगा
 // Development में, यह Vite Dev Server का URL होगा
-const clientURL = process.env.CLIENT_URL || "http://localhost:5173"; // Fallback for dev or missing env var
+const clientURL = process.env.CLIENT_URL || "http://shopnish.vercel.app"; // fallback
+
 
 app.use(
   cors({
