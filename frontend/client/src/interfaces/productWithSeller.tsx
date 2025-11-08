9588c8b6e7c410c73d49dc6568c18f3086e43d46
aapani-dukan <shivraj8404@gmail.com>
2025-11-09 02:06:16 +0530

    Update productWithSeller.tsx

--- frontend/client/src/interfaces/productWithSeller.tsx
+++ frontend/client/src/interfaces/productWithSeller.tsx
@@ -1,7 +1,7 @@
 // frontend/client/src/interfaces/ProductWithSeller.ts
 
-import { Product } from './Product'; // मान लें कि Product यहाँ परिभाषित है
-import { Seller } from './Seller';   // मान लें कि Seller यहाँ परिभाषित है
+import { Product, Seller } from '../pages/DeliveryOrderList.tsx'; // मान लें कि Product यहाँ परिभाषित है
+
 
 export interface ProductWithSeller extends Product {
   seller: Seller;
