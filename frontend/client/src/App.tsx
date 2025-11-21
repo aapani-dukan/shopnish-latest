4537e552f50cc2137a54818341950b75a28b8d43
aapani-dukan <shivraj8404@gmail.com>
2025-11-21 15:49:16 +0530

    Update App.tsx

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -46,7 +46,7 @@ import SellerProfileEdit from '@/components/seller/SellerProfileEdit';
 import AdminProductDetailsPage from './pages/admin/AdminProductDetailsPage'; 
 //import LocationDisplay from "./components/LocationDisplay"; // <-- LocationDisplay को इम्पोर्ट करें
 
-import DeliverySettingsPage from "@/compinents/seller/DeliverySettingsPage"; // Capitalized
+import DeliverySettingsPage from "@/components/seller/DeliverySettingsPage"; // Capitalized
 
 function App() {
   const [isCartModalOpen, setIsCartModalOpen] = useState(false);
