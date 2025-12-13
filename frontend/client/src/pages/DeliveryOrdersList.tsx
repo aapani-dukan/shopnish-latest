7ed13e9125b1739773680c6cad53878a18f302fc
aapani-dukan <shivraj8404@gmail.com>
2025-12-14 01:26:43 +0530

    Update DeliveryOrdersList.tsx

--- frontend/client/src/pages/DeliveryOrdersList.tsx
+++ frontend/client/src/pages/DeliveryOrdersList.tsx
@@ -312,7 +312,7 @@ const OrderItems: React.FC<{ items: OrderItem[] }> = ({ items }) => (
 
     const canUpdateStatus = [
       "assigned",
-      "out_for_pickup",
+      "ready_for_pickup",
       "picked_up",
       "out_for_delivery",
     ].includes(mainStatus);
