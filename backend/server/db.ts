f0d9c2151bc1b97896363c06d3d9fe7284c67bf6
aapani-dukan <shivraj8404@gmail.com>
2025-12-19 07:38:07 +0530

    Update db.ts

--- backend/server/db.ts
+++ backend/server/db.ts
@@ -3,8 +3,8 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 // 🛑 ध्यान दें: यहाँ '*' हटाकर केवल '{ schema }' (curly braces के साथ) इम्पोर्ट करें
-import { schema } from "../shared/backend/schema.ts"; 
-
+//import { schema } from "../shared/backend/schema.ts"; 
+import * as allSchema from "../shared/backend/schema";
 import "dotenv/config";
 
 const connectionString = process.env.DATABASE_URL;
@@ -16,6 +16,6 @@ const pool = new Pool({
 
 // ✅ अब यहाँ 'schema' को सीधे पास करें
 // क्योंकि 'schema' खुद अब टेबल्स और रिलेशंस का combined ऑब्जेक्ट है
-export const db = drizzle(pool, { schema, logger: true });
+export const db = drizzle(pool, { schema: allSchema.schema, logger: true });
 
 export const databasePool = pool;
