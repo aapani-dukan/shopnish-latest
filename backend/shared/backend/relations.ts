dcd0ce10ec110cb75a4fe8e2b759c3ee8b0f8b90
aapani-dukan <shivraj8404@gmail.com>
2025-12-19 09:14:21 +0530

    Update relations.ts

--- backend/shared/backend/relations.ts
+++ backend/shared/backend/relations.ts
@@ -114,7 +114,7 @@ export const ordersRelations = relations(orders, ({ many, one }) => ({
   }),
   subOrders: many(subOrders),
   deliveryBatches: many(deliveryBatches),
-  tracking: many(orderTracking),
+  //tracking: many(orderTracking),
   reviews: many(reviews),
  // orderItems: many(orderItems),
 }));
@@ -176,11 +176,11 @@ export const orderTrackingRelations = relations(orderTracking, ({ one }) => ({
     references: [orders.id],
    // optional: true,
   }),
-  //deliveryBatch: one(deliveryBatches, {
-  //  fields: [orderTracking.deliveryBatchId],
- //   references: [deliveryBatches.id],
-  //  optional: true,
-//  }),
+  deliveryBatch: one(deliveryBatches, {
+   fields: [orderTracking.deliveryBatchId],
+  references: [deliveryBatches.id],
+   optional: true,
+ }),
   updatedBy: one(users, {
     fields: [orderTracking.updatedBy],
     references: [users.id],
